39c39d984e37229f76ea682d9ab8a00612e40f09
Vincent Fragola <vfragola@pcr-net.net>
2018-01-10 09:55:10 -0500

    First Commit

---
+++ TestGitIgnore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitIgnore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.Read();
+        }
+    }
+}
